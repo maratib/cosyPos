14f363225b2c44df81720601677948e91c675b09
Maratib Ali Khan <maratib@gmail.com>
2023-11-06 21:47:44 +0500

    rightpane added

---
+++ components/RightPane.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="space-y-3 mb-6">
+    <div class="bg-white/20 px-4 py-6 rounded-lg flex gap-2 w-full">
+      <div class="bg-white w-7 h-6 rounded-full flex justify-center items-center">1</div>
+      <div class="flex justify-between w-full text-white">
+        <span class="text-white">Roast Chicken <span class="text-white/40 ml-1"> x2</span></span>
+        <span>$25.50</span>
+      </div>
+    </div>
+    <div class="bg-white/20 px-4 py-6 rounded-lg flex gap-2 w-full">
+      <div class="bg-white w-7 h-6 rounded-full flex justify-center items-center">2</div>
+      <div class="flex justify-between w-full text-white">
+        <span class="text-white">Red caviar <span class="text-white/40 ml-1"> x3</span></span>
+        <span>$36.90</span>
+      </div>
+    </div>
+    <div class="bg-white/20 px-4 py-6 rounded-lg flex gap-2 w-full">
+      <div class="bg-white w-7 h-6 rounded-full flex justify-center items-center">3</div>
+      <div class="flex justify-between w-full text-white">
+        <span class="text-white">German sausage<span class="text-white/40 ml-1"> x1</span></span>
+        <span>$5.60</span>
+      </div>
+    </div>
+    <div class="bg-white/20 px-4 py-6 rounded-lg flex gap-2 w-full">
+      <div class="bg-white w-7 h-6 rounded-full flex justify-center items-center">4</div>
+      <div class="flex justify-between w-full text-white">
+        <span class="text-white">Irish cream coffee<span class="text-white/40 ml-1"> x1</span></span>
+        <span>$4.20</span>
+      </div>
+    </div>
+  </div>
+  <div class="bg-white/20 w-full px-4 py-6 rounded-lg text-white">
+    <div class="flex justify-between mb-4">
+      <span class="text-white/80">Subtotal</span>
+      <span>$171.50</span>
+    </div>
+    <div class="flex justify-between border-b border-dashed border-white/40 pb-5 mb-5">
+      <span class="text-white/80">Tax 10%</span>
+      <span>$17.15</span>
+    </div>
+    <div class="flex justify-between mb-32">
+      <span class="text-2xl">Total</span>
+      <span class="text-2xl">$188.65</span>
+    </div>
+    <div class="mb-4">
+      <p class="text-white/40">Payment Method</p>
+      <div class="flex flex-wrap gap-2 w-full justify-center">
+        <div class="flex flex-col items-center justify-center">
+          <div class="border border-white/40 px-6 py-4 mb-2">
+
+          </div>
+          <span>Cash</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+
+
